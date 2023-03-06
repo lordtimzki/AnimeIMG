cc5b0c45f23c4320a0081f2176fb968590ea5260
Timothy Dacalos <tdacalos@uci.edu>
2023-03-05 22:36:20 -0800

    finished api

---
+++ WorkoutProject4.py
@@ -0,0 +1,7 @@
+#deals with input
+import api
+import ui
+
+
+if __name__ == "__main__":
+    print(api.get_random_anime_img())
---
+++ api.py
@@ -0,0 +1,17 @@
+#api
+import json
+import urllib.request
+import sys
+
+
+def get_random_anime_img():
+    url = "https://api.waifu.im/search/"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML,\
+like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request)
+    response_data = response.read()
+    response.close()
+    
+    details = json.loads(response_data)
+    return details
